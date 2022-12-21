5318f0c8cb89602856aff5ff2b3d60be9578091a
Kenny <67345281+Kool-aid1@users.noreply.github.com>
2022-12-20 22:12:00 -0800

    EJ ttc cleanup

--- ticTacToePage.java
+++ ticTacToePage.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -7,18 +9,17 @@ import javax.swing.event.DocumentListener;
 public class ticTacToePage extends JPanel implements ActionListener{
 
     private JLabel game;
-    private JButton back, newGame, 
-                    button1, button2, button3, 
-                    button4, button5, button6, 
-                    button7, button8, button9;
+    private JButton back, newGame;
+    private JButton button_panel = new JButton();
+    private JButton[] buttons = new JButton[9];
+    Random r = new Random();
+    boolean player;
+                    
 
     public ticTacToePage(){
 
         JPanel main = new JPanel();
         JPanel title = new JPanel();
-        JPanel topRow = new JPanel();
-        JPanel midRow = new JPanel();
-        JPanel botRow = new JPanel();
         JPanel players = new JPanel();
         JPanel controls = new JPanel();
 
@@ -26,59 +27,30 @@ public class ticTacToePage extends JPanel implements ActionListener{
         title.add(game);
 
         //buttons to chose which square to set x/o
-        button1 = new JButton(" ");
-        button1.addActionListener(this);
-        button1.setPreferredSize(new Dimension(100,100));
+        button_panel.setLayout(new GridLayout(3,3));
+        //button_panel.setBackground(new Color(0,255,255));//make color to cyan
 
-        button2 = new JButton(" ");
-        button2.addActionListener(this);
-        button2.setPreferredSize(new Dimension(100,100));
-
-        button3 = new JButton(" ");
-        button3.addActionListener(this);
-        button3.setPreferredSize(new Dimension(100,100));
-
-        button4 = new JButton(" ");
-        button4.addActionListener(this);
-        button4.setPreferredSize(new Dimension(100,100));
-
-        button5 = new JButton(" ");
-        button5.addActionListener(this);
-        button5.setPreferredSize(new Dimension(100,100));
-
-        button6 = new JButton(" ");
-        button6.addActionListener(this);
-        button6.setPreferredSize(new Dimension(100,100));
-
-        button7 = new JButton(" ");
-        button7.addActionListener(this);
-        button7.setPreferredSize(new Dimension(100,100));
-
-        button8 = new JButton(" ");
-        button8.addActionListener(this);
-        button8.setPreferredSize(new Dimension(100,100));
-
-        button9 = new JButton(" ");
-        button9.addActionListener(this);
-        button9.setPreferredSize(new Dimension(100,100));
+        for(int i = 0; i < 9; i++){//to fill the 9 buttons for tic tac toe
+            buttons[i] = new JButton(" ");// fill each button
+            button_panel.add(buttons[i]);// add/push the button
+            buttons[i].setFont(new Font("Comic Sans MS",Font.BOLD,120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+            buttons[i].setPreferredSize(new Dimension(100,100));
+        }
 
         //control buttons
         back = new JButton("Back");
         back.addActionListener(this);
         newGame = new JButton("New Game");
 
-        topRow.add(button1);    topRow.add(button2);    topRow.add(button3);
-        midRow.add(button4);    midRow.add(button5);    midRow.add(button6);
-        botRow.add(button7);    botRow.add(button8);    botRow.add(button9);
-
+        //formatting
+        controls.add(button_panel);
         controls.add(back);
         controls.add(newGame);
 
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
         main.add(title);
-        main.add(topRow);
-        main.add(midRow);
-        main.add(botRow);
         main.add(controls);
 
         add(main);
@@ -92,9 +64,9 @@ public class ticTacToePage extends JPanel implements ActionListener{
             controller.getInstance().changeCard("Main Page");
         }
         
-        if(e.getSource() == button1){
-            button1.setText("X");
-        }
+        /*if(e.getSource() == newGame){
+            ticTacToePage t = new ticTacToePage();    
+        }*/
        
     }
 }
